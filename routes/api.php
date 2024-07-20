65aa8aaa70ecc7ad5a00ad0787daafc43c5e3779
Copy Code ID <copycodeid2296@gmail.com>
2024-07-20 12:08:48 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-// Auth::loginUsingId(1);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('user', Controllers\Auth\AuthenticatedUserController::class);
