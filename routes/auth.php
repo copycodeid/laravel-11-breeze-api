95ac87ace97343dd5986e61c40640fb95ab65263
Copy Code ID <copycodeid2296@gmail.com>
2024-07-20 12:05:31 +0800

    Grouping route on `routes/auth.php`

--- routes/auth.php
+++ routes/auth.php
@@ -3,38 +3,37 @@
 use App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [Auth\RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
+// Guest Route
+Route::middleware(['guest'])->group(function () {
+    Route::post('register', [Auth\RegisteredUserController::class, 'store'])
+        ->name('register');
 
-Route::post('login', [Auth\AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
+    Route::post('login', [Auth\AuthenticatedSessionController::class, 'store'])
+        ->name('login');
 
-Route::post('forgot-password', [Auth\PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.email');
+    Route::post('forgot-password', [Auth\PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
 
-Route::post('reset-password', [Auth\NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
+    Route::post('reset-password', [Auth\NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
 
-Route::get('verify-email/{id}/{hash}', Auth\VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
-    ->name('verification.verify');
+// Auth Route
+Route::middleware(['auth'])->group(function () {
+    Route::get('verify-email/{id}/{hash}', Auth\VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
-Route::post('email/verification-notification', [Auth\EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
+    Route::post('email/verification-notification', [Auth\EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['throttle:6,1'])
+        ->name('verification.send');
 
-Route::post('logout', [Auth\AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
+    Route::post('logout', [Auth\AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
 
-Route::patch('update-account', Auth\UpdateAccountController::class)
-    ->middleware('auth')
-    ->name('update-account');
+    Route::patch('update-account', Auth\UpdateAccountController::class)
+        ->name('update-account');
 
-Route::patch('update-password', Auth\UpdatePasswordController::class)
-    ->middleware('auth')
-    ->name('update-password');
+    Route::patch('update-password', Auth\UpdatePasswordController::class)
+        ->name('update-password');
+});
